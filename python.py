0f09ec42c8e958b9afac47bdbe288d6afbd43db3
WK-Shim <aaashim090125@gmail.com>
2022-09-16 18:41:35 +0900

    220916

---
+++ python.py
@@ -0,0 +1,16 @@
+# PS C:\Users\user> python ./dua/python.py
+# Python was not found; run without arguments to install from the Microsoft Store, or disable this shortcut from Settings > Manage App Execution Aliases.
+# ㅠㅠ
+
+# 아마도 저만 실행 할 수 있는 것 같아요
+# /python.py python.py
+# 고양이좋아
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]         #x축
+y = [1, 4, 9, 16, 25]       #y축
+
+plt.plot(x, y)
+plt.show()
+
+#실행된거 보이시나요?
