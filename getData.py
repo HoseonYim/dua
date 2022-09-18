d4b40dd6e912eb1df29ca87c253482ca21d280f1
dacoev <justin.yim14@gmail.com>
2022-09-18 22:25:36 +0900

    getData

---
+++ getData.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+userData = pd.read_csv('User1.csv')
+addicted = userData['Addicted']
+print(userData)
--- solution.py
+++ solution.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
-userData = pd.read_csv('Users.csv')
-characters = userData.drop(columns=['Addicted', 'UserName'])
-addicted = userData['Addicted']
+usersData = pd.read_csv('Users.csv')
+characters = usersData.drop(columns=['Addicted', 'UserName'])
+addicted = usersData['Addicted']
+
+print(characters)
 
 userModel = DecisionTreeClassifier()
 userModel.fit(characters.values, addicted.values)
 
 #tree.export_graphviz(userModel, out_file='userModelData.dot', feature_names=['Age', 'Gender', 'UserTime'], class_names=sorted(addicted.unique()), label='all', rounded='True', filled='True')
 
-predcition = userModel.predict([[26, 1, 11.7], [23, 1, 1.7]])
+predcition = userModel.predict_proba([[26, 1, 11.7], [23, 1, 1.7]])
 
 print(predcition)
